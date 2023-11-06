09e2275cda57985d5ea3f7e3f3259f9811e99541
VV01T3K <wojteks.access@gmail.com>
2023-11-06 17:11:31 +0100

    init2

--- src/web/index.php
+++ src/web/index.php
@@ -8,7 +8,38 @@
 </head>
 
 <body>
-    <?php phpinfo(); ?>
+
+    <?php
+    phpinfo();
+
+    function get_db()
+    {
+        $mongo = new MongoDB\Client(
+            "mongodb://localhost:27017/wai"
+            ,
+            [
+                'username' => 'wai_web'
+                ,
+                'password' => 'w@i_w3b',
+            ]
+        );
+        $db = $mongo->wai;
+        return $db;
+    }
+
+    $db = get_db();
+    //dokument opisujący produkt w sklepie:
+    $product = [
+        'name' => 'Laptop XYZ',
+        'price' => 2999,
+        'manufacturer' => 'ABC',
+        'description' => 'Ładny laptop.'
+    ];
+    //zapis do kolekcji products:
+    $db->products->insertOne($product);
+
+
+    ?>
 
 </body>
 
