20b29c50207ad9b8666217766f749b807899fc95
VV01T3K <wojteks.access@gmail.com>
2023-11-15 23:58:32 +0100

    update

--- src/views/search.php
+++ src/views/search.php
@@ -1,7 +1,7 @@
 <main id="grid">
     <h1>Search</h1>
 
-    <input type="search" hx-vals="{'page': false}" name=" phrase" placeholder="Begin Typing To Search Users..."
+    <input type="search" hx-vals="{'page': false}" name="phrase" placeholder="Begin Typing To Search Users..."
         hx-post="/search" hx-trigger="input changed delay:150ms, search" hx-target="#response">
 
     <div id="response">
