5eea9ee4a064f5231796513c8c7c88056e7c3f5d
VV01T3K <wojteks.access@gmail.com>
2023-11-16 12:34:09 +0100

    404

--- src/views/httpResponse.php
+++ src/views/httpResponse.php
@@ -1,6 +1,15 @@
 <main>
-    <h1>
-        <?= $params['code'] ?> |
-        <?= $params['message'] ?>
-    </h1>
+    <section>
+        <h1>
+            <?= $params['code'] ?>
+        </h1>
+        <h2>
+            <?= $params['message'] ?>
+        </h2>
+    </section>
+    <?php if ($params['code'] == "404"): ?>
+        <img id="splash404" src="static/Img/svg/undraw_404.svg" alt="<?= $params['message'] ?>" />
+    <?php else: ?>
+        <img id="splash404" src="static/Img/svg/undraw_lighthouse.svg" alt="<?= $params['message'] ?>" />
+    <?php endif ?>
 </main>
